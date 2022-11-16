496642d45665843faee4040b88a20abd8a9c2f19
hashir2412 <hashir2412@gmail.com>
2022-11-16 16:39:32 -0600

    modified xpath.java

--- Project7/src/xpath.java
+++ Project7/src/xpath.java
@@ -1,68 +1,47 @@
-import javax.xml.parsers.*;
-import org.w3c.dom.*;
+// https://stackoverflow.com/questions/38207465/how-to-use-and-operator-in-xpath
+
 import java.net.URL;
 
-public class xpath {
-    static void printTitles(Node e) {
-        System.out.println("Titles of all MATH courses that are taught in room LIB 204 are as follows -");
-        NodeList childs = e.getChildNodes();
-        for (int k = 0; k < childs.getLength(); k++) {
-            Node n = childs.item(k);
-            if (n.hasChildNodes()) {
-                if (((Element) n).getElementsByTagName("subj").item(0).getTextContent().equalsIgnoreCase("MATH")) {
-                    Element place = (Element) ((Element) n).getElementsByTagName("place").item(0);
-                    if (place.getElementsByTagName("building").item(0).getTextContent().equalsIgnoreCase("LIB") &&
-                            place.getElementsByTagName("room").item(0).getTextContent().equalsIgnoreCase("204")) {
-                        System.out.println(((Element) n).getElementsByTagName("title").item(0).getTextContent());
-                    }
-                }
-            }
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.*;
+import org.w3c.dom.*;
+
+class XPATH {
+    static void print(Node e) {
+        if (e instanceof Text)
+            System.out.println(((Text) e).getData());
+        else {
+            NodeList c = e.getChildNodes();
+            for (int k = 0; k < c.getLength(); k++)
+                print(c.item(k));
         }
-        System.out.println("X------------------------X-------------------------------X");
     }
 
-    static void printInstructors(Node e) {
-        System.out.println("Instructor name who teaches MATH 412 are as follows -");
-        NodeList childs = e.getChildNodes();
-        for (int k = 0; k < childs.getLength(); k++) {
-            Node n = childs.item(k);
-            if (n.hasChildNodes()) {
-                if (((Element) n).getElementsByTagName("subj").item(0).getTextContent().equalsIgnoreCase("MATH")
-                        && ((Element) n).getElementsByTagName("crse").item(0).getTextContent()
-                                .equalsIgnoreCase("412")) {
-                    System.out.println(((Element) n).getElementsByTagName("instructor").item(0).getTextContent());
-
-                }
-            }
-        }
-        System.out.println("X------------------------X-------------------------------X");
+    static void eval(String query, Document doc) throws Exception {
+        XPathFactory xpathFactory = XPathFactory.newInstance();
+        XPath xpath = xpathFactory.newXPath();
+        NodeList result = (NodeList) xpath.evaluate(query, doc, XPathConstants.NODESET);
+        System.out.println("XPath query: " + query);
+        for (int i = 0; i < result.getLength(); i++)
+            print(result.item(i));
+        System.out.println();
     }
 
-    static void printTitlesFromWieting(Node e) {
-        System.out.println("Titles of all courses taught by Wieting are as follows -");
-        NodeList childs = e.getChildNodes();
-        for (int k = 0; k < childs.getLength(); k++) {
-            Node n = childs.item(k);
-            if (n.hasChildNodes()) {
-                if (((Element) n).getElementsByTagName("instructor").item(0).getTextContent()
-                        .equalsIgnoreCase("Wieting")) {
-                    System.out.println(((Element) n).getElementsByTagName("title").item(0).getTextContent());
-
-                }
-            }
-        }
-        System.out.println("X------------------------X-------------------------------X");
-    }
-
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db
                 .parse((new URL("http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/courses/reed.xml"))
                         .openStream());
-        Node root = doc.getDocumentElement();
-        printTitles(root);
-        printInstructors(root);
-        printTitlesFromWieting(root);
+        System.out.println("Titles of all MATH courses that are taught in room LIB 204 are as follows:");
+        eval("//course[subj='MATH' and place/building='LIB' and place/room='204']/title", doc);
+        System.out.println("X--------------------------------X--------------------------------X");
+        System.out.println("Instructor name who teaches MATH 412 are as follows:");
+        eval("//course[subj='MATH' and crse='412']/instructor", doc);
+        System.out.println("X--------------------------------X--------------------------------X");
+        System.out.println("Titles of all courses taught by Wieting are as follows:");
+        eval("//course[instructor='Wieting']/title", doc);
+        System.out.println("X--------------------------------X--------------------------------X");
     }
 }
